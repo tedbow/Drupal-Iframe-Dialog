0d2c27ea2d8fe0e9100361578430453703e554ac
Ted Bowman <ted.git@tedbow.com>
2017-02-03 14:52:12 -0500

    init

---
+++ js/dialog.iframe.js
@@ -0,0 +1,49 @@
+/**
+ * @file
+ * Drupal's off-canvas library.
+ *
+ * @todo This functionality should extracted into a new core library or a part
+ *  of the current drupal.dialog.ajax library.
+ *  https://www.drupal.org/node/2784443
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+  /**
+   * Toggle the js-outside-edit-mode class on items that we want to disable while in edit mode.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Toggle the js-outside-edit-mode class.
+   */
+  Drupal.behaviors.tester = {
+    attach: function () {
+      // Create the element if needed.
+
+      $('#i-frame-dialog #edit-submit').on('click', function (event) {
+        event.preventDefault();
+        if ($('#drupal-offcanvas').length) {
+          return;
+        }
+        var path = $('#i-frame-dialog #edit-path').val();
+        var $dialog = $('<div id="drupal-offcanvas" class="ui-front"><iframe src="http://www.octo2.dev/d8_2_ux/' + path + '?_wrapper_format=ajax" /></div>').appendTo('body');
+        var dialogOptions = {
+          'dialogClass':"ui-dialog-outside-in ui-dialog-offcanvas",
+          'modal': false,
+          'autoResize': false,
+          'resizable': 'w',
+          'draggable': false,
+          'drupalAutoButtons': false,
+          'buttons': []
+
+        };
+        var dialog = Drupal.dialog($dialog.get(0), dialogOptions);
+        dialog.show();
+
+      });
+    }
+  };
+
+})(jQuery, Drupal);
---
+++ src/Form/IFrameDialog.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\iframe_dialog\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class IFrameDialog.
+ *
+ * @package Drupal\iframe_dialog\Form
+ */
+class IFrameDialog extends FormBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'i_frame_dialog';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Path'),
+      '#description' => $this->t('Enter url to open'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    ];
+    $form['go'] = [
+      '#type' => 'submit',
+      '#title' => $this->t('Go'),
+    ];
+    $form['#attached']['library'][] = 'iframe_dialog/drupal.iframe_dialog';
+    $form['#attached']['library'][] = 'outside_in/drupal.off_canvas';
+
+    $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+    * {@inheritdoc}
+    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Display result.
+    foreach ($form_state->getValues() as $key => $value) {
+        drupal_set_message($key . ': ' . $value);
+    }
+
+  }
+
+}
